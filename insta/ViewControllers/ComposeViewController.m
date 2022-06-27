72f0589b66386e39bc5390977acfb0a8ce43919e
Kaylyn Phan <kaylynphan@fb.com>
2022-06-27 16:08:13 -0700

    uploading default image and caption to server

--- insta/ViewControllers/ComposeViewController.m
+++ insta/ViewControllers/ComposeViewController.m
@@ -6,10 +6,13 @@
 //
 
 #import "ComposeViewController.h"
+#import "Post.h"
 
 @interface ComposeViewController ()
 - (IBAction)didTapCancel:(id)sender;
 - (IBAction)didTapShare:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *captionField;
+@property (weak, nonatomic) IBOutlet UIImageView *photoImage;
 
 @end
 
@@ -30,10 +33,55 @@
 }
 */
 
-- (IBAction)didTapShare:(id)sender {
+
+// tap gesture recognizer
+- (IBAction)imageTapped:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+
+    // The Xcode simulator does not support taking pictures, so let's first check that the camera is indeed supported on the device before trying to present it.
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    // Do something with the images (based on your use case)
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (IBAction)didTapCancel:(id)sender {
     [self dismissViewControllerAnimated:true completion:nil];
 }
+
+- (IBAction)didTapShare:(id)sender {
+    //get an image and caption
+    UIImage *myImage = self.photoImage.image;
+    NSString *myCaption = self.captionField.text;
+    
+    [Post postUserImage:myImage withCaption:myCaption withCompletion:^(BOOL succeeded, NSError * _Nullable error){
+        if (error != nil) {
+            NSLog(@"Error posting image: %@", error.localizedDescription);
+        } else {
+            NSLog(@"Posted image!");
+        }
+    }];
+    
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
 @end
